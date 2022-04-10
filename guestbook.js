27aaf2f776ff55ae1df2b02fe32aa44a7b646c31
karl-off <karl.off68@gmail.com>
2022-04-10 19:07:09 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://8f4e1ce8.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
